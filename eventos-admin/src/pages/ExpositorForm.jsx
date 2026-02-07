ac02cb9e236c2029a2c150a40a326425fa0bee10
Felipe Carvalho <atendimentodrywall7@gmail.com>
2026-02-07 01:05:03 -0300

    Resolve

--- eventos-admin/src/pages/ExpositorForm.jsx
+++ eventos-admin/src/pages/ExpositorForm.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { getEvento, listExpositores, createExpositor, updateExpositor } from "../services/api";
 
 export default function ExpositorForm() {
